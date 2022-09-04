1b478496466c0c71e19be8397f5621b67aef8618
jmccallister93 <jaredmccallister93@gmail.com>
2022-09-04 14:42:20 -0700

    Started the form

---
+++ js/monster.js
@@ -0,0 +1,19 @@
+var monsterForm = document.querySelector("#monster_form");
+
+monsterForm.addEventListener('change', (event) => {
+    
+    var customTypeInput = document.createElement("input");
+    customTypeInput.setAttribute('id',"custom_monster_type");
+
+    var monsterType = document.getElementById("monster_type").value;
+
+    if(monsterType === 'custom' ){
+        monsterForm.appendChild(customTypeInput);
+    } 
+    else if(monsterType !== 'custom') {
+        document.getElementById('custom_monster_type').remove();
+    }
+})
+
+
+
