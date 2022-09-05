0baad11a12bf0c816442230a5c2a351d95fda165
jmccallister93 <jaredmccallister93@gmail.com>
2022-09-05 12:38:26 -0700

    Form formatting

--- js/monster.js
+++ js/monster.js
@@ -15,5 +15,31 @@
 //     }
 // })
 
+// Button collapse
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+
+    for(let j=0; j<coll.length;j++){
+        var btnValue = coll[j].textContent;
+        if(btnValue === "Show"){
+            btnValue = "Hide";
+            coll[j].textContent = btnValue;
+        } else if (btnValue === "Hide"){
+            btnValue = "Show";
+            coll[j].textContent = btnValue;
+        }
+    }
+   
+  });
+}
 
