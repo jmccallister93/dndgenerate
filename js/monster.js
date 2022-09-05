86524ab65fcdc6daab13b867457f0a1b6716bd61
jmccallister93 <jaredmccallister93@gmail.com>
2022-09-05 14:48:51 -0700

    Continue adding display

--- js/monster.js
+++ js/monster.js
@@ -77,4 +77,102 @@ monsterAlignment.addEventListener("change", (e) => {
     displayAlignment.innerText = capArr;
 })
 
+var monsterAC = document.getElementById("monster-ac");
+var displayAC = document.getElementById("ac-display");
+monsterAC.addEventListener("change", (e) => {
+    displayAC.innerText = monsterAC.value;
+})
 
+
+var monsterACType = document.getElementById("monster-ac-type");
+var displayACType = document.getElementById("ac-type-display");
+monsterACType.addEventListener("change", (e) => {
+    var arr = monsterACType.value.split(" ");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    var capArr = arr.join(" ");
+    displayACType.innerText = capArr;
+})
+
+var monsterShield = document.getElementById("shield");
+var displayShield = document.getElementById("shield-display");
+monsterShield.addEventListener("change", (e) => {
+    if (e.target.checked){
+        displayShield.innerText = "Yes";
+    } else{
+        displayShield.innerText = "None";
+    }
+})
+
+var monsterHP = document.getElementById("monster-hp");
+var displayHP = document.getElementById("hp-display");
+monsterHP.addEventListener("change", (e) => {
+    displayHP.innerText = monsterHP.value;
+})
+
+var monsterSpeed = document.getElementById("speed");
+var displaySpeed = document.getElementById("speed-display");
+monsterSpeed.addEventListener("change", (e) => {
+    displaySpeed.innerText = monsterSpeed.value;
+})
+
+var monsterBurrow = document.getElementById("burrow-speed");
+var displayBurrow = document.getElementById("burrow-display");
+monsterBurrow.addEventListener("change", (e) => {
+    displayBurrow.innerText = monsterBurrow.value;
+})
+
+var monsterClimb = document.getElementById("climb-speed");
+var displayClimb = document.getElementById("climb-display");
+monsterClimb.addEventListener("change", (e) => {
+    displayClimb.innerText = monsterClimb.value;
+})
+
+var monsterFly = document.getElementById("fly-speed");
+var displayFly = document.getElementById("fly-display");
+monsterFly.addEventListener("change", (e) => {
+    displayFly.innerText = monsterFly.value;
+})
+
+var monsterSwim = document.getElementById("swim-speed");
+var displaySwim = document.getElementById("swim-display");
+monsterSwim.addEventListener("change", (e) => {
+    displaySwim.innerText = monsterSwim.value;
+})
+
+var monsterStr = document.getElementById("monster-str");
+var displayStr = document.getElementById("str-display");
+monsterStr.addEventListener("change", (e) => {
+    displayStr.innerText = monsterStr.value;
+})
+
+var monsterDex = document.getElementById("monster-dex");
+var displayDex = document.getElementById("dex-display");
+monsterDex.addEventListener("change", (e) => {
+    displayDex.innerText = monsterDex.value;
+})
+
+var monsterCon = document.getElementById("monster-con");
+var displayCon = document.getElementById("con-display");
+monsterCon.addEventListener("change", (e) => {
+    displayCon.innerText = monsterCon.value;
+})
+
+var monsterInt = document.getElementById("monster-int");
+var displayInt = document.getElementById("int-display");
+monsterInt.addEventListener("change", (e) => {
+    displayInt.innerText = monsterInt.value;
+})
+
+var monsterWis = document.getElementById("monster-wis");
+var displayWis = document.getElementById("wis-display");
+monsterWis.addEventListener("change", (e) => {
+    displayWis.innerText = monsterWis.value;
+})
+
+var monsterCha = document.getElementById("monster-cha");
+var displayCha = document.getElementById("cha-display");
+monsterCha.addEventListener("change", (e) => {
+    displayCha.innerText = monsterCha.value;
+})
