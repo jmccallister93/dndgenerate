ff913f520d412a1b03dcc4c33da26215e9ce38a1
jmccallister93 <jaredmccallister93@gmail.com>
2022-09-07 00:25:19 -0700

    Don't know what to do for monster...

--- js/monster_v2.js
+++ js/monster_v2.js
@@ -88,3 +88,18 @@ monsterAlignment.addEventListener("change", (e) => {
     displayAlignment.innerText = capArr;
 })
 
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
