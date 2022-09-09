1d83ab4c31c4ee0b1681b5b4feeedb00af4e9f8c
jmccallister93 <jaredmccallister93@gmail.com>
2022-09-08 21:38:41 -0700

    still working on adding armor

--- js/5eapi.js
+++ js/5eapi.js
@@ -2,43 +2,25 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://www.dnd5eapi.co/api/spells/', true)
+request.open('GET', 'https://api.open5e.com/armor', true)
 
 request.onload = function () {
   // Begin accessing JSON data here
     var data = JSON.parse(this.response);
-    // data = data.results;
-    
-    if (request.status >= 200 && request.status < 400) {
-        data.results.forEach(spell => {
-            const card = document.createElement('div')
-            card.setAttribute('class', 'card-test')
-
-            const h1 = document.createElement('h1')
-            h1.textContent = spell.name
-            
-            const p =document.createElement('p')
-            // spell.url = spell.url.substrin(0,200)
-            p.textContent = `${spell.url}`
-            
-            container.appendChild(card)
-
-            card.appendChild(h1)
-            card.appendChild(p)
-            
+        
+        data.results.forEach(armors => {
+          var armor = armors.name
+          var armorName = []
+          armorName.push(armor)
+          var list = document.getElementById('root')
+          list.append(armorName)  
         });
-      } else {
-        const errorMessage = document.createElement('marquee')
-        errorMessage.textContent = `Gah, it's not working!`
-        app.appendChild(errorMessage)
-      }
-}
+    }
 
 // Send request
 request.send()
 
 
-const app = document.getElementById('root')
-const container = document.createElement('div')
-container.setAttribute('class', 'container-test')
-app.appendChild(container)
+
+// var push = document.getElementsByClassName('.push-here')
+// push.appendChild(armor)
